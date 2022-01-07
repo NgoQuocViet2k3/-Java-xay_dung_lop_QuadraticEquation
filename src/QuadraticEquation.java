91e9b8ad2beddc6c85efec214ae1237c3a771715
ruukichi <ngovietlc93@gmail.com>
2022-01-07 14:37:05 +0700

    xay lung lop phuong trinh bac hai

---
+++ src/QuadraticEquation.java
@@ -0,0 +1,63 @@
+public class QuadraticEquation {
+    private double a;
+    private double b;
+    private double c;
+
+
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return this.a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return this.b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return this.c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public double getDiscriminant() {
+        return Math.pow(this.b, 2) - 4 * this.a * this.c;
+    }
+
+    public double getRoot1() {
+        return -b + Math.sqrt(getDiscriminant()) / (2 * a);
+    }
+
+    public double getRoot2() {
+        return -b - Math.sqrt(getDiscriminant()) / (2 * a);
+    }
+
+    public void result() {
+        if (a == 0) {
+            System.out.println("Phương trình bậc 1");
+        } else {
+            if (getDiscriminant() < 0) {
+                System.out.println("Phương trình vô nghiệm");
+            } else if (getDiscriminant() == 0) {
+                System.out.println("Phương trình có nghiệm kép x = " + (-b / (2 * a)));
+            } else {
+                System.out.println("Phương trình có 2 nghiệm x1 = " + getRoot1() + ", x2 = " + getRoot2());
+            }
+        }
+    }
+
+}
